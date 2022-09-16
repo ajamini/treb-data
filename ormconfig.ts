aca88dd1584fea35e924f7a60ce9bf09f2a39c01
iraj <iraj.amini@outlook.com>
2022-09-16 15:16:09 -0400

    added fixing dependencies

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,12 +1,18 @@
 import { DataSource } from 'typeorm';
+import * as fs from 'fs';
 
 export const connectionSource = new DataSource({
   type: 'postgres',
-  host: process.env.DBHOST ?? 'localhost',
-  port: process.env.DBPORT ? parseInt(process.env.DBPORT) : 5432,
-  username: process.env.DBUSER ?? 'postgres',
-  password: process.env.DBPASS ?? 'mysecretpassword',
+  host: process.env.DBHOST || 'localhost',
+  port: parseInt(process.env.DBPORT || '5432'),
+  username: process.env.DBUSER || 'postgres',
+  password: process.env.DBPASS || 'mysecretpassword',
   database: 'db-postgres',
+  extra: {
+    ssl: {
+      ca: fs.readFileSync('./ca-certificate.crt'),
+    },
+  },
   entities: ['src/**.entity.ts'],
   migrations: ['migration/*.ts'],
   migrationsRun: false,
