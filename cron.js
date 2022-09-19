6d003af2caeae6c8c54419602e8eb6f0b4c8cabf
iraj <iraj.amini@outlook.com>
2022-09-19 10:39:49 -0400

    adding dockerfile

--- cron.js
+++ cron.js
@@ -3,18 +3,7 @@ const { exec } = require("child_process");
 
 // Schedule tasks to be run on the server.
 cron.schedule('* * * * *', function() {
-  console.log('running a task every minute');
-  exec('date', (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
+  console.log('.');
 });
 
 cron.schedule('12,15,18,20 * * * *', function() {
